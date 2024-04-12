a1e99ac69991143de7143cd51b332c8598151776
Javier Espert Real <javi.espert@gmail.com>
2024-04-12 18:07:24 +0100

    apperror: fix messed up comments

--- apperror/too_many_requests.go
+++ apperror/too_many_requests.go
@@ -10,12 +10,12 @@ func (e tooManyRequestsErr) TooManyRequests() bool {
 	return true
 }
 
-// TooManyRequests creates a new tooManyRequests error.
+// TooManyRequests creates a new too many requests error.
 func TooManyRequests(msg string) error {
 	return &tooManyRequestsErr{error: errors.New(msg)}
 }
 
-// IsTooManyRequests checks if the error is a tooManyRequests error.
+// IsTooManyRequests checks if the error is a too many requests error.
 func IsTooManyRequests(err error) bool {
 	var target interface {
 		TooManyRequests() bool

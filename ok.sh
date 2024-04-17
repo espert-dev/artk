a6d6494612d0d9ec0d358de8f49bee35dac82095
Javier Espert Real <javi.espert@gmail.com>
2024-04-17 09:21:40 +0100

    Fix reporting of JUnit tests
    Only tests from the last module were being recorded.

--- ok.sh
+++ ok.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+# Run the script at the root of the repo.
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Ensure that the artifact output directories exist.
+mkdir -p .test
+
+# Remove JUnit reports from previous runs.
+find .test -type f -delete
+
 build_module() {
-    go build "$1/..."
+    module_root="$1"
+    go build "./${module_root}/..."
 }
 
 test_module() {
-    gotestsum --junitfile junit.xml -- \
-        -timeout=1m -failfast -cover -race "$1/..."
+    module_root="$1"
+    gotestsum --junitfile ".test/${module_root/\//-}.junit.xml" -- \
+        -timeout=1m -failfast -cover -race "./${module_root}/..."
 }
 
-for module in ./core ./tech/*; do
-    echo -e "\e[1;34mModule $module\e[0m"
-    build_module "$module"
-    test_module "$module"
+for module_root in core tech/*; do
+    echo -e "\e[1;34mModule ${module_root}\e[0m"
+    build_module "$module_root"
+    test_module "$module_root"
     echo
 done
 

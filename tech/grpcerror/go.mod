d914a621ddd72ff342ecbe156e85ec19e5e5cc6d
Javier Espert Real <javi.espert@gmail.com>
2024-04-22 07:58:02 +0100

    Implement package tech/grpcerror

--- tech/grpcerror/go.mod
+++ tech/grpcerror/go.mod
@@ -3,14 +3,14 @@ module artk.dev/tech/grpcerror
 go 1.22.0
 
 require (
-	artk.dev/core v0.0.0-00010101000000-000000000000
-	github.com/stretchr/testify v1.9.0
+	artk.dev/core v0.0.0-20240419141500-98bbd2dc1469
+	google.golang.org/grpc v1.63.2
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240415180920-8c6c420018be // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 )
 
 replace artk.dev/core => ../../core
--- tech/grpcerror/grpcerror.go
+++ tech/grpcerror/grpcerror.go
@@ -1,7 +1,91 @@
+// Package grpcerror provides serialization and deserialization of errors
+// that follow apperror conventions.
+//
+// When in doubt about code mappings, check this:
+// https://github.com/grpc/grpc/blob/master/doc/http-grpc-status-mapping.md
 package grpcerror
 
-import "artk.dev/core/apperror"
+import (
+	"artk.dev/core/apperror"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
-func CreateFakeDependency(err error) int {
-	return int(apperror.KindOf(err))
+// Encode an application error into a gRPC error.
+func Encode(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	kind := apperror.KindOf(err)
+	code := EncodeKind(kind)
+	return status.Error(code, err.Error())
+}
+
+// Decode a gRPC error into an application error.
+func Decode(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	s, ok := status.FromError(err)
+	if !ok {
+		// Will be handled as an unknown error.
+		return apperror.Unknown("cannot parse gRPC error: %w", err)
+	}
+
+	kind := DecodeKind(s.Code())
+	return apperror.New(kind, s.Message())
+}
+
+// EncodeKind encodes an apperror.Kind into a gRPC codes.Code.
+func EncodeKind(kind apperror.Kind) codes.Code {
+	switch kind {
+	case apperror.OK:
+		return codes.OK
+	case apperror.ValidationError:
+		return codes.InvalidArgument
+	case apperror.UnauthorizedError:
+		return codes.Unauthenticated
+	case apperror.ForbiddenError:
+		return codes.PermissionDenied
+	case apperror.NotFoundError:
+		return codes.NotFound
+	case apperror.ConflictError:
+		return codes.AlreadyExists
+	case apperror.PreconditionFailedError:
+		return codes.FailedPrecondition
+	case apperror.TooManyRequestsError:
+		return codes.Unavailable
+	case apperror.TimeoutError:
+		return codes.DeadlineExceeded
+	default:
+		return codes.Unknown
+	}
+}
+
+// DecodeKind decodes a codes.Code into an apperror.Kind.
+func DecodeKind(code codes.Code) apperror.Kind {
+	switch code {
+	case codes.OK:
+		return apperror.OK
+	case codes.InvalidArgument:
+		return apperror.ValidationError
+	case codes.Unauthenticated:
+		return apperror.UnauthorizedError
+	case codes.PermissionDenied:
+		return apperror.ForbiddenError
+	case codes.NotFound:
+		return apperror.NotFoundError
+	case codes.AlreadyExists:
+		return apperror.ConflictError
+	case codes.FailedPrecondition:
+		return apperror.PreconditionFailedError
+	case codes.Unavailable:
+		return apperror.TooManyRequestsError
+	case codes.DeadlineExceeded:
+		return apperror.TimeoutError
+	default:
+		return apperror.UnknownError
+	}
 }

6e37e23e09f4b545f2c8389ef97e32e702265d15
Javier Espert Real <javi.espert@gmail.com>
2024-04-25 08:53:36 +0100

    Restore linter testableexamples and change examples for typetraits

--- typetraits/typetraits.go
+++ typetraits/typetraits.go
@@ -1,18 +1,40 @@
 // Package typetraits provides traits that can be used to restrict behaviour.
 package typetraits
 
-// NoCopy can be embedded into a struct to warn against copy.
-type NoCopy struct{}
+// NoCopy can be embedded into a struct to protect against copying.
+// Any copies will be flagged by the copylocks analysis in go vet.
+//
+// Example:
+//
+//	type T struct {
+//		typetraits.NoCompare
+//	}
+//
+//	x := T{} // Vetting error!
+type NoCopy struct {
+	// Use an anonymous field to prevent leaking methods.
+	_ noCopy
+}
 
-func (n *NoCopy) Lock() {
+type noCopy struct{}
+
+func (n *noCopy) Lock() {
 	// Do nothing.
 }
 
-func (n *NoCopy) Unlock() {
+func (n *noCopy) Unlock() {
 	// Do nothing.
 }
 
 // NoCompare can be embedded into a struct to prevent comparison.
+//
+// Example:
+//
+//	type T struct {
+//		typetraits.NoCompare
+//	}
+//
+//	T{} := T{} // Compilation error!
 type NoCompare struct {
 	_ [0]func()
 }

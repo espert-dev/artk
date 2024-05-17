cbcfddeb8fdb817fde44fb6ed3f36dd236b9b25a
Javier Espert Real <javi.espert@gmail.com>
2024-05-17 21:43:44 +0100

    eventmux: implement Shutdown

--- eventmux/mux.go
+++ eventmux/mux.go
@@ -8,10 +8,15 @@ import (
 
 var _ Observer[any] = (&Mux[any]{}).Observe
 
-// Mux is an in-memory event multiplexer.
+// Mux is a thread-safe in-memory event multiplexer.
 type Mux[Event any] struct {
-	mutex     sync.RWMutex
-	observers []Observer[Event]
+	mutex sync.RWMutex   // 24 bytes on 64 bits.
+	wg    sync.WaitGroup // 12 bytes on 64 bits.
+
+	// On 64-bit systems, 4 bytes of padding will be inserted here to
+	// ensure that 64-bit words remain aligned.
+
+	observers []Observer[Event] // 24 bytes on 64 bits.
 }
 
 // Observe and propagate an event to registered observers.
@@ -19,8 +24,11 @@ func (m *Mux[Event]) Observe(ctx context.Context, event Event) error {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
 
+	numObservers := len(m.observers)
+	m.wg.Add(numObservers)
+
 	// Observers are notified concurrently.
-	for i := range len(m.observers) {
+	for i := range numObservers {
 		// Trade performance for safety.
 		event := clone.Of(event)
 
@@ -29,6 +37,8 @@ func (m *Mux[Event]) Observe(ctx context.Context, event Event) error {
 			observer Observer[Event],
 			event Event,
 		) {
+			defer m.wg.Done()
+
 			// Call the observer.
 			//
 			// While we ultimately ignore the error here, it was
@@ -53,6 +63,26 @@ func (m *Mux[Event]) WillNotify(
 
 	// Chaining improves DX.
 	return m
+}
+
+// Shutdown the Mux and communicate finishing via the sync.WaitGroup.
+func (m *Mux[Event]) Shutdown(wg *sync.WaitGroup) {
+	// Synchronously prevent new messages from being sent.
+	m.stopEventPropagation()
+
+	// Asynchronously wait for tasks to finish.
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		m.wg.Wait()
+	}()
+}
+
+func (m *Mux[Event]) stopEventPropagation() {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	m.observers = nil
 }
 
 // New creates a Mux.

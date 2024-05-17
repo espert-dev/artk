0c7af39bd6faedd54a7b0dbbd4f9a3e0eb834c6f
Javier Espert Real <javi.espert@gmail.com>
2024-05-17 22:23:58 +0100

    eventmux: add None

---
+++ eventmux/none.go
@@ -0,0 +1,22 @@
+package eventmux
+
+import "context"
+
+// None is an event handler that does nothing.
+//
+// If you have a service that supports listeners, inject None by default during
+// construction, and you can stop checking for `nil` before notifying them.
+//
+// Example:
+//
+//	// Construction.
+//	h.listener = eventmux.None[MyEvent]
+//
+//	// Usage (no need to check if `listener` is nil).
+//	h.listener(ctx, e)
+func None[Event any](_ context.Context, _ Event) error {
+	// Deliberately do nothing.
+	return nil
+}
+
+var _ Observer[any] = None[any]

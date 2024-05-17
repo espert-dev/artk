dda9b9d5480182537f33c93621fe0b54e577f8dc
Javier Espert Real <javi.espert@gmail.com>
2024-05-17 20:26:45 +0100

    racechecker: add tests
    I didn't see how to do it on the first try.

--- racechecker/require_disabled.go
+++ racechecker/require_disabled.go
@@ -5,5 +5,6 @@ package racechecker
 // Require skips the test if the race condition checker is not enabled.
 // It might save some cycles, but it mostly serves as live documentation.
 func Require(t testingT) {
+	t.Helper()
 	t.Skip("This test requires the race checker. Skipping.")
 }
---
+++ racechecker/require_disabled_test.go
@@ -0,0 +1,20 @@
+//go:build !race
+
+package racechecker_test
+
+import (
+	"artk.dev/racechecker"
+	"testing"
+)
+
+func TestRequire(t *testing.T) {
+	var fakeT T
+	racechecker.Require(&fakeT)
+
+	if n := fakeT.NumCallsToHelper; n != 1 {
+		t.Errorf("expected 1 call to Helper, got %v", n)
+	}
+	if n := fakeT.NumCallsToSkip; n != 1 {
+		t.Errorf("expected 1 call to Skip, got %v", n)
+	}
+}

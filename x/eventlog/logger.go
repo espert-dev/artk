bf5ee9e6116f9b914976f4b5d59cffe1be58a4d1
Javier Espert Real <javi.espert@gmail.com>
2024-05-18 17:45:12 +0100

    eventlog: logging middleware for eventmux-compatible brokers

---
+++ x/eventlog/logger.go
@@ -0,0 +1,52 @@
+package eventlog
+
+import (
+	"artk.dev/eventmux"
+	"context"
+	"fmt"
+	"log/slog"
+)
+
+func LoggerMiddleware[Event any](
+	logger *slog.Logger,
+) func(eventmux.Observer[Event]) eventmux.Observer[Event] {
+	// Pre-generate the event type at initialization type, avoiding its
+	// re-computation on every event.
+	var example Event
+	eventType := fmt.Sprintf("%T", example)
+	logger = logger.With(slog.String(eventTypeKey, eventType))
+
+	return func(next eventmux.Observer[Event]) eventmux.Observer[Event] {
+		return func(ctx context.Context, e Event) error {
+			logger = logger.With(slog.Any(eventKey, e))
+			logger.LogAttrs(
+				ctx,
+				slog.LevelDebug,
+				"eventmux: notifying observer",
+			)
+
+			err := next(ctx, e)
+			if err == nil {
+				logger.LogAttrs(
+					ctx,
+					slog.LevelDebug,
+					"eventmux: success",
+				)
+			} else {
+				logger.LogAttrs(
+					ctx,
+					slog.LevelError,
+					"eventmux: failure",
+					slog.String(errorKey, err.Error()),
+				)
+			}
+
+			// Middleware must propagate errors.
+			return err
+		}
+	}
+}
+
+const eventTypeKey = "event"
+const eventKey = "eventType"
+const errorKey = "error"
